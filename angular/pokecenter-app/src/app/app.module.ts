61668caccca8042bfb049656441f1c76c94ab006
aaknox <azhya.knox@gmail.com>
2020-12-06 20:18:30 -0600

    routing setup finished

--- angular/pokecenter-app/src/app/app.module.ts
+++ angular/pokecenter-app/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PasswordRecoveryComponent } from './components/password-recovery/password-recovery.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordRecoveryComponent,
+    RegistrationComponent,
+    HomeComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
